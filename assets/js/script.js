a1e0cc890dd32f33aaca435e7b4151ccc4a40d5c
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-03-21 12:18:31 +0000

    add presentQuestion initial logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,39 @@
 const startButton = document.getElementById("start-button");
 const startContainer = document.getElementById("start-container");
+const mainElement = document.querySelector("main");
 
 const removeStartContainer = () => {
   startContainer.remove();
 };
 
 const presentQuestion = () => {
-  console.log("present question");
+  //create new HTML elements and set attributes
+  const questionDiv = document.createElement("div");
+  questionDiv.setAttribute("class", "question-container");
+  const questionHeader = document.createElement("h1");
+  const questionButton1 = document.createElement("button");
+  questionButton1.setAttribute("id", "button-1");
+  const questionButton2 = document.createElement("button");
+  questionButton1.setAttribute("id", "button-2");
+  const questionButton3 = document.createElement("button");
+  questionButton1.setAttribute("id", "button-3");
+  const questionButton4 = document.createElement("button");
+  questionButton1.setAttribute("id", "button-4");
+
+  //set element text content
+  questionHeader.textContent = "Question 1";
+  questionButton1.textContent = "Button 1";
+  questionButton2.textContent = "Button 2";
+  questionButton3.textContent = "Button 3";
+  questionButton4.textContent = "Button 4";
+
+  //append children onto DOM tree
+  mainElement.appendChild(questionDiv);
+  questionDiv.appendChild(questionHeader);
+  questionDiv.appendChild(questionButton1);
+  questionDiv.appendChild(questionButton2);
+  questionDiv.appendChild(questionButton3);
+  questionDiv.appendChild(questionButton4);
 };
 
 const startGame = () => {
