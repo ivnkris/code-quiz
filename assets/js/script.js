29d2043f7a8bc05931e95451fe8c0b58701917d6
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-03-21 14:14:47 +0000

    add presentQuestions logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,7 +49,10 @@ const questionsObject = {
 };
 
 //timer default value
-let timer = 60;
+let timer = 5;
+
+//question tracker default value
+let questionTracker = 1;
 
 //removes the content of the container displayed before starting the game
 const removeStartContainer = () => {
@@ -57,34 +60,38 @@ const removeStartContainer = () => {
 };
 
 //renders the questions on the user's screen
-const presentQuestion = () => {
-  //create new HTML elements and set attributes
-  const questionDiv = document.createElement("div");
-  questionDiv.setAttribute("class", "question-container");
-  const questionHeader = document.createElement("h1");
-  const questionButton1 = document.createElement("button");
-  questionButton1.setAttribute("id", "button-1");
-  const questionButton2 = document.createElement("button");
-  questionButton1.setAttribute("id", "button-2");
-  const questionButton3 = document.createElement("button");
-  questionButton1.setAttribute("id", "button-3");
-  const questionButton4 = document.createElement("button");
-  questionButton1.setAttribute("id", "button-4");
+const presentQuestions = () => {
+  if (questionTracker <= 5) {
+    const questionSelector = `question${questionTracker}`;
 
-  //set element text content
-  questionHeader.textContent = "Question 1";
-  questionButton1.textContent = "Button 1";
-  questionButton2.textContent = "Button 2";
-  questionButton3.textContent = "Button 3";
-  questionButton4.textContent = "Button 4";
+    //create new HTML elements and set attributes
+    const questionDiv = document.createElement("div");
+    questionDiv.setAttribute("class", "question-container");
+    const questionHeader = document.createElement("h1");
+    const questionButton1 = document.createElement("button");
+    questionButton1.setAttribute("id", "button-1");
+    const questionButton2 = document.createElement("button");
+    questionButton1.setAttribute("id", "button-2");
+    const questionButton3 = document.createElement("button");
+    questionButton1.setAttribute("id", "button-3");
+    const questionButton4 = document.createElement("button");
+    questionButton1.setAttribute("id", "button-4");
 
-  //append children onto DOM tree
-  mainElement.appendChild(questionDiv);
-  questionDiv.appendChild(questionHeader);
-  questionDiv.appendChild(questionButton1);
-  questionDiv.appendChild(questionButton2);
-  questionDiv.appendChild(questionButton3);
-  questionDiv.appendChild(questionButton4);
+    //set element text content
+    questionHeader.textContent = questionsObject[questionSelector].question;
+    questionButton1.textContent = questionsObject[questionSelector].answer1;
+    questionButton2.textContent = questionsObject[questionSelector].answer2;
+    questionButton3.textContent = questionsObject[questionSelector].answer3;
+    questionButton4.textContent = questionsObject[questionSelector].answer4;
+
+    //append children onto DOM tree
+    mainElement.appendChild(questionDiv);
+    questionDiv.appendChild(questionHeader);
+    questionDiv.appendChild(questionButton1);
+    questionDiv.appendChild(questionButton2);
+    questionDiv.appendChild(questionButton3);
+    questionDiv.appendChild(questionButton4);
+  }
 };
 
 //ends the game and presents the score
@@ -112,12 +119,12 @@ const startGame = () => {
   //remove original game content
   removeStartContainer();
 
-  //present first question
-  presentQuestion();
-
   //start the timer
   timerElement.textContent = timer;
   startTimer();
+
+  //present questions
+  presentQuestions();
 };
 
 //on click of the start button, run the startGame function
