a5f59456612d74f79456be11cddb8a1722c17b55
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-03-21 11:59:19 +0000

    wired up start button in js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const startButton = document.getElementById("start-button");
+
+const startGame = () => {
+  console.log("START");
+};
+
+//On click of the start button, run the startGame function
+startButton.addEventListener("click", startGame);
